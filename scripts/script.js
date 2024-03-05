8d4516c68eab263f51b740d574197feba82178ae
Vitor Mateus <vmateusaguiar@gmail.com>
2024-03-05 15:23:13 -0300

    feat: funcionalidade descriptografar

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,12 @@ function criptografar() {
     const board = document.querySelector('.content__container__board');
     board.innerHTML = new_text;
 }
+
+function descriptografar() {
+    const text_textarea = document.querySelector('.content__section__textarea').value;
+
+    const new_text = text_textarea.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+
+    const board = document.querySelector('.content__container__board');
+    board.innerHTML = new_text;
+}
