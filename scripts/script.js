1f0960c5dd7b213c2e3519afd46b5d12ac12f46b
Vitor Mateus <vmateusaguiar@gmail.com>
2024-03-07 11:02:49 -0300

    feat: funcionalidade copiar

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,37 @@
 function criptografar() {
-    const text_textarea = document.querySelector('.content__section__textarea').value;
+    const textarea = document.querySelector('.content__section__textarea');
 
-    const new_text = text_textarea.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    const text = textarea.value;
+
+    const new_text = text.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
 
     const board = document.querySelector('.content__container__board');
     board.innerHTML = new_text;
+    textarea.value = '';
 }
 
 function descriptografar() {
-    const text_textarea = document.querySelector('.content__section__textarea').value;
+    const textarea = document.querySelector('.content__section__textarea');
 
-    const new_text = text_textarea.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    const text = textarea.value;
+
+    const new_text = text.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
 
     const board = document.querySelector('.content__container__board');
     board.innerHTML = new_text;
+    textarea.value = '';
+}
+
+function copy() {
+    const board = document.querySelector('.content__container__board');
+    const text = board.textContent;
+
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        // console.log('Texto copiado com sucesso!');
+    })
+    .catch((e) => {
+        // console.error('Erro!', e);
+    })
+    board.innerHTML = '';
 }
